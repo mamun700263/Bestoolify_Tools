841cb598c6dc7f616ece2f19d79a74d85521c572
MD ABDULLAH ALL MAMUN <mamun700263@gmail.com>
2026-04-02 10:25:26 +0100

    feature: import moved to function

--- backend/app/tasks/google_map_scraper.py
+++ backend/app/tasks/google_map_scraper.py
@@ -1,26 +1,21 @@
 import asyncio
-
 from app.core import Logger
 from app.core.celery import celery_app
 from app.core.data_exporters import FileSaver
-from app.scrapers import google_map_scraper as scraper
 
 logger = Logger.get_logger(__name__, "google_map")
-logger.info("Scraping started")
-
 
 @celery_app.task(
-        bind=True,
-        name="map_scraper"
-    )
+    bind=True,
+    name="map_scraper"
+)
 def run_scraper(self, query: str, file_name: str):
+    from app.scrapers import google_map_scraper as scraper
+    
     logger.info(f"started search for: {query}, saving as {file_name}")
     try:
         logger.info(f"Starting scrape for: {query}")
         data = scraper(query)
-        # data = asyncio.run(scraper(query))
-        # FileSaver.save(data, file_name)
-        # logger.info(f"[SUCCESS] Saved results to {file_name}")
         return data
     except Exception as e:
         logger.error(f"Error scraping {query}: {e}")
