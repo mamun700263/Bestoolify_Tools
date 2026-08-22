d3aeb729661f5b5979fdd8ee96077d017a05db26
MD ABDULLAH ALL MAMUN <mamun700263@gmail.com>
2026-08-22 02:13:24 +0100

    refactor #70: creates central testing system

--- backend/tests/uptime_keeper/test_ping.py
+++ backend/tests/uptime_keeper/test_ping.py
@@ -3,5 +3,10 @@ import pytest
 from app.uptime_keeper.ping import normalize_url
 
 
-def test_normalize_url():
-    pass
+@pytest.mark.asyncio
+async def test_normalize_url():
+    valid_url = "https://www.google.com"
+
+    result = await normalize_url(valid_url)
+
+    assert result == valid_url
